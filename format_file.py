a28966339f824773df1ed32e4af4129f75734a83
Kuldeepsoni84 <92205631+Kuldeepsoni84@users.noreply.github.com>
2023-11-01 15:31:17 +0530

    Update format_file.py

--- format_file.py
+++ format_file.py
@@ -11,11 +11,11 @@ def list_text(l):
             s=s+','+str(i)
     return s[1::]
 def header(f,page):    
-    f.writelines(['SECONDARY SCHOOL CERTIFICATE EXAMINATION 2022'.center(143,' ')])
+    f.writelines(['SECONDARY SCHOOL CERTIFICATE EXAMINATION 2023'.center(143,' ')])
     f.write('\n')
     f.writelines(['**CBSE-66/ CENTRE MEMO**'.center(143,' ')])
     f.write('\n')
-    f.write(' Region : NOIDA'.ljust(34,' ')+'CENTRE-  841515 :: SUDITI GLOBAL ACADEMY NAGARIYA MAINPURI UP'.ljust(98,' ')+'PAGE : '+str(page))
+    f.write(' Region : NOIDA'.ljust(34,' ')+'CENTRE-  841259 :: SUDITI GLOBAL ACADEMY NAGARIYA MAINPURI UP'.ljust(98,' ')+'PAGE : '+str(page))
     f.write('\n')
     f.write('|'+'-'*141+'|')
     f.write('\n')
@@ -27,19 +27,19 @@ def header(f,page):
     #f.write('\n')
     #f.write('|'.ljust(12,' ')+''.ljust(26,' ')+''.ljust(22,' ')+'|'.ljust(37,' ')+'|'.ljust(20,' ')+'|'.ljust(25,' ')+'|')
     f.flush()
-def footer(f):
+def footer(f,fname,sname,ftsname,stsname):
     f.write('\n')
     f.write('| **Certified that the No. of Answer Books indicated in Col-6 have been checked and packed for dispatch'.ljust(142,' ')+'|')
     f.write('\n')
     f.write('| Withnes of two asstt. Supdt.(one should be from the school other than exam centre'.ljust(142,' ')+'|')
     f.write('\n')
-    f.write('| 1 Signature  :.......................  2. Signature  :......................'.ljust(142,' ')+'|')
+    f.write('| 1 Signature  :..................................................  2. Signature  :..................................................'.ljust(142,' ')+'|')
     f.write('\n')
-    f.write('|   Name       :.......................     Name       :......................'.ljust(142,' ')+'|')
+    f.write(('|   Name       :'+fname.upper().ljust(50,'.')+'     Name       :'+sname.upper().ljust(50,'.')).ljust(142,' ')+'|')
     f.write('\n')
-    f.write('|   Designation:.......................     Designation:......................'.ljust(142,' ')+'|')
+    f.write('|   Designation:TEACHER...........................................     Designation:TEACHER...........................................'.ljust(142,' ')+'|')
     f.write('\n')
-    f.write('|   Address    :.......................     Address    :......................'.ljust(142,' ')+'|')
+    f.write(('|   Address    :'+ftsname.upper().ljust(50,'.')+'     Address    :'+stsname.upper().ljust(50,'.')).ljust(142,' ')+'|')
     f.write('\n')
     f.write('|'+' '*141+'|')
     f.write('\n')
@@ -51,7 +51,7 @@ def footer(f):
     f.write('\n')
     f.write('|'+'SIGNATURE OF THE CENTRE SUPDT.'.rjust(141,' ')+'|')
     f.write('\n')
-    f.write('|'+'RUBBER STAMP AND DATE'.rjust(141,' ')+'|')
+    f.write('|'+'RUBBER STAMP AND DATE '.rjust(141,' ')+'|')
     f.write('\n')
     f.write('|'+'-'*141+'|')
     f.write('\f')
@@ -72,21 +72,20 @@ def middle(file,total_stu,absent_stu,present_stu):
     file.write('\n')
     file.write('|'.ljust(60,'-')+'|'.ljust(37,'-')+'|'.ljust(20,'-')+'|'.ljust(25,'-')+'|')
 def series(file,absent_stu):
-    rt=csv.reader(file)
-    print(type(rt))
-    next(rt)
-    first_record=next(rt)
-    f=first_record[0]
+    #rt=csv.reader(file)
+    #next(rt)
+    first_record=file[0]
+    f=file[0]
     break_point=roundup(int(f))
     x=[]
     c_roll=0
     l=[f]
     ser=[]
     abs_lst=[]
-    for i in rt:    
-        if (int(i[0])-int(f))==1 and int(i[0])<=break_point :
-            l.append(i[0])
-            f=i[0]
+    for i in file[1:]:    
+        if (int(i)-int(f))==1 and int(i)<=break_point :
+            l.append(i)
+            f=i
         else:
             '''for j in absent_stu:
                 if int(j) >= int(l[0]) and int(j)<=int(l[-1]):
@@ -94,9 +93,9 @@ def series(file,absent_stu):
         
             ser.append([l[0],l[-1]])
             l.clear()
-            break_point=roundup(int(i[0])+1)
-            l.append(i[0])
-            f=i[0]
+            break_point=roundup(int(i)+1)
+            l.append(i)
+            f=i
     
    
     else:
